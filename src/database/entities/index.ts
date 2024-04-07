acbf100f5dc9dc8899681499c8257f59543a720a
kaungkhantzawdev <kaungkhantzaw.dev@gmail.com>
2024-04-07 13:06:31 +0700

    setup

---
+++ src/database/entities/index.ts
@@ -0,0 +1,1 @@
+export { Users } from '@database/entities/users.entity';
---
+++ src/database/entities/users.entity.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Users {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+  })
+  name: string;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+  })
+  email: string;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+  })
+  password: string;
+
+  @Column({
+    type: 'int',
+  })
+  credit: number;
+}
